edc583476c362ea73ec59df762ce67333a681fa5
luolin <1071893649@qq.com>
2017-12-28 17:05:58 +0800

    去掉window不支持的命令

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,19 @@ const babel = require('gulp-babel')
 const execSync = require('child_process').execSync
 const merge2 = require('merge2');
 
-execSync('rm -rf lib')
-execSync('mkdir -p ./lib/assets')
-execSync('cp -r ./src/assets/. ./lib/assets')
+// execSync('rm -rf lib')
+// execSync('mkdir -p ./lib/assets')
+// execSync('cp -r ./src/assets/. ./lib/assets')
 
-gulp.task('compile', function () {
-    const tsCss = gulp.src('src/components/**/*.less')
-        .pipe(less())
-        .pipe(gulp.dest('./lib/components'))
-    const tsVue = gulp.src('src/components/**/*.vue')
-        .pipe(vueify())
-        .pipe(gulp.dest('./lib/components'))
-    const tsJs = gulp.src('src/components/**/*.js')
-        .pipe(babel())
-        .pipe(gulp.dest('./lib/components'))
-    return merge2([tsVue, tsCss, tsJs])
+gulp.task('compile', function() {
+  const tsCss = gulp.src('src/components/**/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./lib/components'))
+  const tsVue = gulp.src('src/components/**/*.vue')
+    .pipe(vueify())
+    .pipe(gulp.dest('./lib/components'))
+  const tsJs = gulp.src('src/components/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('./lib/components'))
+  return merge2([tsVue, tsCss, tsJs])
 });
